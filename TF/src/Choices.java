c4fa6a1c7abcf1e7fc800b97e3b5d2da236f605e
Gabriel Pinho <pinho.gabriel@edu.pucrs.br>
2024-06-19 00:57:55 -0300

    separando

--- TF/src/Choices.java
+++ TF/src/Choices.java
@@ -1,8 +1,12 @@
 import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.TransferHandler;
 
-public class Choices{
-    private int qntdPinos;
+public final class Choices{
+    private final int qntdPinos;
     private PinoColorido[] pinos;
     private String[] colors = {"RED", "GREEN", "YELLOW", "BLUE", "MAGENTA", "ORANGE"};
     private JPanel panel;
@@ -11,11 +15,24 @@ public class Choices{
         this.qntdPinos = qntdPinos;
         pinos = new PinoColorido[qntdPinos];
         panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        
-        for (String color : colors) {
-            PinoColorido pinos = new PinoColorido(Cores.getInstance().getCor(color));
-            panel.add(pinos);
+        for (int i = 0; i < qntdPinos; i++) {
+            pinos[i] = PinoColorido.criaPinoColorido(colors[i]);
+            panel.add(pinos[i]);   
             
+            DragDrop();
+        }
+    }
+
+    public void DragDrop(){
+        for(int i = 0; i < qntdPinos; i++) {
+            pinos[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent evt) {
+                    JComponent comp = (JComponent) evt.getSource();
+                    TransferHandler handler = comp.getTransferHandler();
+                    handler.exportAsDrag(comp, evt, TransferHandler.COPY);
+                }
+            });
         }
     }
 }
